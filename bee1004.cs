aff6809578af433683f879c001a5a95e638c8b56
Cadu Amorim <86995782+caduamorimm-dev@users.noreply.github.com>
2022-02-26 18:07:32 -0300

    Create bee1004.cs

---
+++ bee1004.cs
@@ -0,0 +1,16 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+    
+      int PROD, A, B;
+      
+          A = Int32.Parse(Console.ReadLine());
+          B = Int32.Parse(Console.ReadLine());
+          PROD = A * B;
+          
+            Console.WriteLine("PROD = {0}", PROD); 
+        
+            }
+        }
