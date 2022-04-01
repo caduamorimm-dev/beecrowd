7cc354f0bd5ebc18dbbef55bda7bdd3c52ed7f92
Cadu Amorim <86995782+caduamorimm-dev@users.noreply.github.com>
2022-04-01 19:16:12 -0300

    Update bee1001.cs

--- bee1001.cs
+++ bee1001.cs
@@ -4,7 +4,9 @@ class URI {
 
     static void Main(string[] args) { 
 
-      int A, B; A = Convert.ToInt32(Console.ReadLine()); B = Convert.ToInt32(Console.ReadLine());
+      int A, B; 
+        A = Convert.ToInt32(Console.ReadLine()); 
+        B = Convert.ToInt32(Console.ReadLine());
       int X = A + B; 
 
       Console.WriteLine("X = "+X);
